71d5b99a5b9927cddaa2ffdb53c12d5c3a5cb1e4
Mary-Ann Horley <maryann.horley@gmail.com>
2018-05-02 22:33:33 +0100

    added function to class.py so it can be called on a schedule by Zappa

--- class.py
+++ class.py
@@ -3,19 +3,18 @@ from models import get_playlist_videos, get_playlist_videos_by_user, get_descrip
 import boto3
 import urllib.request
 
-client = MongoClient('mongodb://maryann:ferrari1357@ds159845.mlab.com:59845/tube')
-db = client['tube']
+def tag_videos():
+    client = MongoClient('mongodb://maryann:ferrari1357@ds159845.mlab.com:59845/tube')
+    db = client['tube']
+    playlists = list(db.users.find({}, {"playlist_url": 1, "user": 1, "_id": 0}))
+    for playlist in playlists:
+        playlist_url = playlist['playlist_url']
+        user = playlist['user']
+        video_list = list(get_playlist_videos_by_user(playlist_url, "50", user))
+        for video in video_list:
+            video_id = video['video_id']
+            user = video['user']
+            tags = get_description_noun_phrases(video_id)
+            db.videos.update( { "video_id": video_id }, {"$set": { "video_id": video_id, "user": user, "tags": tags }}, upsert = True )
 
-# Get list of playlists from database
-playlists = list(db.users.find({}, {"playlist_url": 1, "user": 1, "_id": 0}))
-
-
-for playlist in playlists:
-    playlist_url = playlist['playlist_url']
-    user = playlist['user']
-    video_list = list(get_playlist_videos_by_user(playlist_url, "50", user))
-    for video in video_list:
-        video_id = video['video_id']
-        user = video['user']
-        tags = get_description_noun_phrases(video_id)
-        print(tags)
+        
